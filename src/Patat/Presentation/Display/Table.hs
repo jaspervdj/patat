c24e6252cbb007a488c871194e738bb440f1bd12
Jasper Van der Jeugt <m@jaspervdj.be>
2016-10-01 21:21:32 +0630

    Finish initial table support

--- src/Patat/Presentation/Display/Table.hs
+++ src/Patat/Presentation/Display/Table.hs
@@ -12,7 +12,6 @@ import           Data.List         (intersperse, transpose)
 import           Data.Monoid       ((<>))
 import           Patat.PrettyPrint ((<$$>))
 import qualified Patat.PrettyPrint as PP
-import qualified Text.Pandoc       as Pandoc
 
 
 --------------------------------------------------------------------------------
@@ -27,47 +26,67 @@ data Table = Table
 --------------------------------------------------------------------------------
 prettyTable
     :: Table -> PP.Doc
-prettyTable Table {..}
-    | all (all isSimpleCell) (tHeaders : tRows) =
-        PP.indent (PP.Trimmable "  ") (PP.Trimmable "  ") $
-            lineIf (not isHeaderLess) (hcat2
-                [ PP.dullblue (PP.align w a header)
-                | (w, a, header) <- zip3 columnWidths tAligns tHeaders
-                ]) <>
-            headerSeparator <$$>
-            PP.vcat
-                [ hcat2
-                    [ PP.align w a cell
-                    | (w, a, cell) <- zip3 columnWidths tAligns row
-                    ]
-                | row <- tRows
-                ] <$$>
-            lineIf isHeaderLess headerSeparator <>
-            (if PP.null tCaption
-                then mempty
-                else PP.newline <> "Table: " <> tCaption)
-    | otherwise = PP.string $ show (tHeaders : tRows)
+prettyTable Table {..} = PP.indent (PP.Trimmable "  ") (PP.Trimmable "  ") $
+    lineIf (not isHeaderLess) (hcat2 headerHeight
+        [ PP.dullblue (PP.align w a (vpad headerHeight header))
+        | (w, a, header) <- zip3 columnWidths tAligns tHeaders
+        ]) <>
+    dashedHeaderSeparator columnWidths <$$>
+    joinRows
+        [ hcat2 rowHeight
+            [ PP.align w a (vpad rowHeight cell)
+            | (w, a, cell) <- zip3 columnWidths tAligns row
+            ]
+        | (rowHeight, row) <- zip rowHeights tRows
+        ] <$$>
+    lineIf isHeaderLess (dashedHeaderSeparator columnWidths) <>
+    lineIf
+        (not $ PP.null tCaption) (PP.newline <> "Table: " <> tCaption)
   where
     lineIf cond line = if cond then line <> PP.newline else mempty
 
+    joinRows
+        | all (all isSimpleCell) tRows = PP.vcat
+        | otherwise                    = PP.vcat . intersperse ""
+
     isHeaderLess = all PP.null tHeaders
 
-    headerSeparator = hcat2
-        [ PP.dullmagenta (PP.string (replicate w '-'))
-        | w <- columnWidths
-        ]
-
-    isSimpleCell :: PP.Doc -> Bool
-    isSimpleCell = (<= 1) . fst . PP.dimensions
-
-    hcat2 :: [PP.Doc] -> PP.Doc
-    hcat2 = mconcat . intersperse (PP.string "  ")
-
-    rowDimensions :: [[(Int, Int)]]
-    rowDimensions = map (map PP.dimensions) (tHeaders : tRows)
+    headerDimensions = map PP.dimensions tHeaders :: [(Int, Int)]
+    rowDimensions    = map (map PP.dimensions) tRows :: [[(Int, Int)]]
 
     columnWidths :: [Int]
     columnWidths =
-        [ foldr max 0 (map snd col)
-        | col <- transpose rowDimensions
+        [ safeMax (map snd col)
+        | col <- transpose (headerDimensions : rowDimensions)
         ]
+
+    rowHeights   = map (safeMax . map fst) rowDimensions :: [Int]
+    headerHeight = safeMax (map fst headerDimensions)    :: Int
+
+    vpad :: Int -> PP.Doc -> PP.Doc
+    vpad height doc =
+        let (actual, _) = PP.dimensions doc in
+        doc <> mconcat (replicate (height - actual) PP.newline)
+
+    safeMax = foldr max 0
+
+    hcat2 :: Int -> [PP.Doc] -> PP.Doc
+    hcat2 rowHeight = PP.paste . intersperse (spaces2 rowHeight)
+
+    spaces2 :: Int -> PP.Doc
+    spaces2 rowHeight =
+        mconcat $ intersperse PP.newline $
+        replicate rowHeight (PP.string "  ")
+
+
+--------------------------------------------------------------------------------
+isSimpleCell :: PP.Doc -> Bool
+isSimpleCell = (<= 1) . fst . PP.dimensions
+
+
+--------------------------------------------------------------------------------
+dashedHeaderSeparator :: [Int] -> PP.Doc
+dashedHeaderSeparator columnWidths = mconcat $ intersperse (PP.string "  ")
+    [ PP.dullmagenta (PP.string (replicate w '-'))
+    | w <- columnWidths
+    ]
--- tests/tables.md
+++ tests/tables.md
@@ -16,3 +16,33 @@ Table:  Demonstration of simple table syntax.
     123     123       123          123
       1     1          1             1
 -------     ------ ----------   -------
+
+# Multiline
+
+-------------------------------------------------------------
+ Centered   Default           Right Left
+  Header    Aligned         Aligned Aligned
+----------- ------- --------------- -------------------------
+   First    row                12.0 Example of a row that
+                                    spans multiple lines.
+
+  Second    row                 5.0 Here's another one. Note
+                                    the blank line between
+                                    rows.
+-------------------------------------------------------------
+
+Table: Here's the caption. It, too, may span
+multiple lines.
+
+# Headerless multiline
+
+----------- ------- --------------- -------------------------
+   First    row                12.0 Example of a row that
+                                    spans multiple lines.
+
+  Second    row                 5.0 Here's another one. Note
+                                    the blank line between
+                                    rows.
+----------- ------- --------------- -------------------------
+
+: Here's a multiline table without headers.
