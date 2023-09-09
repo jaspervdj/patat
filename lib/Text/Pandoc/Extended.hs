3a6ab4ac04c1f8a2c6d5ffd958fe397664f496bf
Jasper Van der Jeugt <m@jaspervdj.be>
2023-09-09 15:44:32 +0200

    Support showing plain text files

--- lib/Text/Pandoc/Extended.hs
+++ lib/Text/Pandoc/Extended.hs
@@ -1,18 +1,21 @@
 --------------------------------------------------------------------------------
-{-# LANGUAGE BangPatterns #-}
-{-# LANGUAGE LambdaCase   #-}
+{-# LANGUAGE LambdaCase #-}
 module Text.Pandoc.Extended
     ( module Text.Pandoc
 
     , plainToPara
     , newlineToSpace
+
+    , readPlainText
     ) where
 
 
 --------------------------------------------------------------------------------
+import           Data.Char          (isSpace)
 import           Data.Data.Extended (grecT)
-import           Text.Pandoc
+import qualified Data.Text          as T
 import           Prelude
+import           Text.Pandoc
 
 
 --------------------------------------------------------------------------------
@@ -28,3 +31,18 @@ newlineToSpace = grecT $ \case
     SoftBreak -> Space
     LineBreak -> Space
     inline    -> inline
+
+
+--------------------------------------------------------------------------------
+-- | A plain-text reader.  Always returns empty metadata.
+readPlainText :: T.Text -> Pandoc
+readPlainText = Pandoc mempty . pure . Plain . go
+  where
+    go txt0 = case T.uncons txt0 of
+        Nothing           -> []
+        Just (' ',  txt1) -> Space : go txt1
+        Just ('\r', txt1) -> go txt1
+        Just ('\n', txt1) -> SoftBreak : go txt1
+        Just (c,    txt1) ->
+            let (pre, post) = T.break isSpace txt1 in
+            Str (T.cons c pre) : go post
