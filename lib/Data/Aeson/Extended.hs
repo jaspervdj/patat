217ccca2cdada93d71be57488be519433ebfe430
Jasper Van der Jeugt <m@jaspervdj.be>
2023-10-14 14:41:20 +0200

    Add random transition type

--- lib/Data/Aeson/Extended.hs
+++ lib/Data/Aeson/Extended.hs
@@ -3,12 +3,14 @@ module Data.Aeson.Extended
     ( module Data.Aeson
 
     , FlexibleNum (..)
+
+    , resultToEither
     ) where
 
 import           Data.Aeson
-import qualified Data.Text  as T
+import qualified Data.Text        as T
 import           Prelude
-import           Text.Read  (readMaybe)
+import           Text.Read        (readMaybe)
 
 -- | This can be parsed from a JSON string in addition to a JSON number.
 newtype FlexibleNum a = FlexibleNum {unFlexibleNum :: a}
@@ -19,3 +21,7 @@ instance (FromJSON a, Read a) => FromJSON (FlexibleNum a) where
         Nothing -> fail $ "Could not parse " ++ T.unpack str ++ " as a number"
         Just x  -> return (FlexibleNum x)
     parseJSON val = FlexibleNum <$> parseJSON val
+
+resultToEither :: Result a -> Either String a
+resultToEither (Success x)   = Right x
+resultToEither (Error   err) = Left err
--- lib/Patat/Transition.hs
+++ lib/Patat/Transition.hs
@@ -1,5 +1,9 @@
 --------------------------------------------------------------------------------
+{-# LANGUAGE DeriveFoldable    #-}
+{-# LANGUAGE DeriveFunctor     #-}
+{-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
 module Patat.Transition
     ( Duration (..)
     , threadDelayDuration
@@ -13,14 +17,36 @@ module Patat.Transition
 
 
 --------------------------------------------------------------------------------
-import qualified Data.Aeson                  as A
+import qualified Data.Aeson.Extended         as A
+import qualified Data.Aeson.TH.Extended      as A
+import           Data.Bifunctor              (first)
 import qualified Data.HashMap.Strict         as HMS
+import           Data.List.NonEmpty          (NonEmpty)
+import qualified Data.List.NonEmpty          as NonEmpty
 import           Data.Text                   (Text)
 import qualified Data.Text                   as T
 import           Patat.Presentation.Settings (TransitionSettings (..))
 import qualified Patat.Transition.Dissolve   as Dissolve
 import           Patat.Transition.Internal
 import qualified Patat.Transition.SlideLeft  as SlideLeft
+import           System.Random               (uniformR)
+
+
+--------------------------------------------------------------------------------
+data Random a = Random
+    { rItems :: NonEmpty a
+    } deriving (Foldable, Functor, Traversable)
+
+
+--------------------------------------------------------------------------------
+$(A.deriveFromJSON A.dropPrefixOptions ''Random)
+
+
+--------------------------------------------------------------------------------
+random :: Random TransitionGen -> TransitionGen
+random r size matrix0 matrix1 rg0 =
+    let (idx, rg1) = uniformR (0, length (rItems r) - 1) rg0 in
+    (rItems r NonEmpty.!! idx) size matrix0 matrix1 rg1
 
 
 --------------------------------------------------------------------------------
@@ -35,10 +61,15 @@ transitions = HMS.fromList
 parseTransitionSettings
     :: TransitionSettings -> Either String TransitionGen
 parseTransitionSettings ts = case HMS.lookup ty transitions of
+    _ | ty == "random"  -> do
+        settings <- A.resultToEither . A.fromJSON . A.Object $ tsParams ts
+        tgens <- traverse parseTransitionSettings settings
+        pure $ random tgens
     Nothing             -> Left $ "unknown transition type: " ++ show ty
-    Just (Transition f) -> case A.fromJSON (A.Object $ tsParams ts) of
-        A.Success conf -> Right $ f conf
-        A.Error   err  -> Left $
-            "could not parse " ++ T.unpack ty ++ " transition: " ++ err
+    Just (Transition f) ->
+        fmap (f $) .
+        first (\err ->
+            "could not parse " ++ T.unpack ty ++ " transition: " ++ err) .
+        A.resultToEither . A.fromJSON . A.Object $ tsParams ts
   where
    ty = tsType ts
