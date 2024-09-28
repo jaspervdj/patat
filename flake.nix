b7c3b6d297432dfd2b382a6d7c7067de5bc30e2f
Jasper Van der Jeugt <m@jaspervdj.be>
2024-09-28 14:46:13 +0200

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "patat";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs :
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+     let pkgs = inputs.nixpkgs.legacyPackages.${system};
+         haskell = pkgs.haskell.packages.ghc98;
+     in
+  {
+    packages = {
+      default = haskell.callCabal2nix "patat" ./. {};
+    };
+    devShells = {
+      default = pkgs.mkShell {
+        packages = [
+          pkgs.cabal-install
+          pkgs.entr
+          haskell.goldplate
+          haskell.stylish-haskell
+          (haskell.ghc.withPackages (p: inputs.self.packages.${system}.default.buildInputs))
+        ];
+      };
+    };
+  });
+}
