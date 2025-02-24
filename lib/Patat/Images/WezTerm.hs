26ed54b176deb12c44291d2a04db0781ed6797f6
Deepak Sharma <deepak@deepaksharma.us>
2025-02-24 16:42:29 -0500

    Removed extraneous deps

--- lib/Patat/Images/WezTerm.hs
+++ lib/Patat/Images/WezTerm.hs
@@ -5,24 +5,32 @@
 
 module Patat.Images.WezTerm
     ( backend
+    , Pane
+    , Size
+    , camelToSnake
+    , camelToSnakeCase
+    , wordsOn
     ) where
 
 
 --------------------------------------------------------------------------------
-import           Codec.Picture
-import           Control.Exception         (throwIO)
-import           Control.Monad             (unless, when)
-import qualified Data.Aeson                as A
-import           Data.Aeson.Casing         as AC
-import qualified Data.ByteString           as B
-import qualified Data.ByteString.Base64    as B64
-import qualified Data.ByteString.Lazy.UTF8 as BLU
-import           GHC.Generics              (Generic)
-import           Patat.Cleanup             (Cleanup)
-import qualified Patat.Images.Internal     as Internal
-import           System.Directory
-import           System.Environment        (lookupEnv)
-import           System.Process
+import           Codec.Picture           (DynamicImage,
+                                          Image (imageHeight, imageWidth),
+                                          decodeImage, dynamicMap)
+import           Control.Exception       (throwIO)
+import           Control.Monad           (unless, when)
+import qualified Data.Aeson              as A
+import qualified Data.ByteString         as B
+import qualified Data.ByteString.Base64  as B64
+import           Data.Char               (isUpper, toLower)
+import qualified Data.Text.Lazy          as TL
+import           Data.Text.Lazy.Encoding (encodeUtf8)
+import           GHC.Generics            (Generic)
+import           Patat.Cleanup           (Cleanup)
+import qualified Patat.Images.Internal   as Internal
+import           System.Directory        (findExecutable)
+import           System.Environment      (lookupEnv)
+import           System.Process          (readProcess)
 
 
 --------------------------------------------------------------------------------
@@ -42,7 +50,7 @@ data Pane =
          } deriving (Show, Generic)
 
 instance A.FromJSON Pane where
-    parseJSON = A.genericParseJSON $ AC.aesonPrefix AC.snakeCase
+    parseJSON = A.genericParseJSON $ A.defaultOptions { A.fieldLabelModifier = camelToSnakeCase . drop 4 }
 
 
 --------------------------------------------------------------------------------
@@ -52,7 +60,7 @@ data Size =
          } deriving (Show, Generic)
 
 instance A.FromJSON Size where
-    parseJSON = A.genericParseJSON $  AC.aesonPrefix AC.snakeCase
+    parseJSON = A.genericParseJSON $ A.defaultOptions { A.fieldLabelModifier = camelToSnakeCase . drop 4 }
 
 
 --------------------------------------------------------------------------------
@@ -72,7 +80,7 @@ drawImage path = do
     content <- B.readFile path
 
     wez <- wezExecutable
-    resp <- fmap BLU.fromString $ readProcess wez ["cli", "list", "--format", "json"] []
+    resp <- fmap (encodeUtf8 . TL.pack) $ readProcess wez ["cli", "list", "--format", "json"] []
     let panes = (A.decode resp :: Maybe [Pane])
 
     Internal.withEscapeSequence $ do
@@ -127,6 +135,28 @@ activePaneAspectRatio (Just x) =
 
 --------------------------------------------------------------------------------
 defaultAr :: Double
-defaultAr = (4 / 3 :: Double) -- Good enough for a VT100
+defaultAr = (4 / 3 :: Double)             -- Good enough for a VT100
 
 
+--------------------------------------------------------------------------------
+camelToSnake :: String -> String
+camelToSnake [] = []
+camelToSnake (x:xs)
+  | isUpper x = '_' : toLower x : camelToSnake xs
+  | otherwise = x : camelToSnake xs
+
+
+--------------------------------------------------------------------------------
+camelToSnakeCase :: String -> String
+camelToSnakeCase s = case camelToSnake s of
+  ('_':rest) -> rest                      -- Remove any leading spaces
+  result     -> result
+
+
+--------------------------------------------------------------------------------
+wordsOn :: (Char -> Bool) -> String -> [String]
+wordsOn t s =
+    case dropWhile t s of
+        "" -> []
+        s' -> w : wordsOn t s''
+            where (w, s'') = break t s'
