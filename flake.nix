16636b40675963398eacd6eb55aaf47e2dfe1688
Deniz Alp Durmaz <denalp@proton.me>
2025-03-09 00:17:33 +0200

    Bump nixpkgs pin
    In order to bring in GHC984, also update `inputs.nixpkgs.url` format
    to use a trailing slash to define the ref/branch.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "patat";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
